2c9de711536083d2fa2661a09816dd944f2411fd
VinayGoudCheguri <85886420+VinayGoudCheguri@users.noreply.github.com>
2021-07-22 17:10:36 +0530

    Add files via upload

--- Problem_38.java
+++ Problem_38.java
@@ -1,11 +1,6 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Problem_38 {
-    public static class Count {
-        int count = 0;
-    }
-
     private static boolean IsSafe(int[][] chess_board, int row, int col) {
         int i, j;
         // lying on same column
@@ -26,61 +21,36 @@ public class Problem_38 {
         return true;
     }
 
-    private static void FindAllArrangements(int[][] chess_board, Count ans,int row,int col) {
-        int N = chess_board[0].length;
-        if(row==N){
-            if(col==0){
-                return;
+    private static int FindAllArrangements(int[][] chess_board, int row, int col) {
+        int count = 0;
+        int N = chess_board.length;
+        for(int i = 0;i<N;i++){
+            chess_board[i][0]=1;
+            if(Solve(chess_board,1)){
+                count++;
             }
-            else {
-                row = FindLastQueenRow(chess_board, col-1);
-                chess_board[row][col-1]=0;
-                FindAllArrangements(chess_board, ans, row+1, col - 1);
-            }
+            chess_board[i][0]=0;
         }
-        // After each possible complete arrangement
-        if(IsSafe(chess_board,row,col)){
-            chess_board[row][col]=1;
-            if(col==N-1){
-                ans.count++;
-                row = FindLastQueenRow(chess_board,0);
-                Initialize(chess_board);
-                FindAllArrangements(chess_board,ans,row+1,0);
-            }
-            else{
-                FindAllArrangements(chess_board,ans,0,col+1);
-            }
-        }
-        else{// If Queen cannot be added
-            if(row<N-1){
-                FindAllArrangements(chess_board,ans,row+1,col);
-            }
-            else if(row==N-1){
-                if(col==0){
-                    return;
+        return  count;
+    }
+
+    private static boolean Solve(int[][] chess_board, int col) {
+        int N = chess_board.length;
+        for(int i=0;i<N;i++){
+            if(IsSafe(chess_board,i,col)) {
+                if(col==N-1){
+                    return true;
                 }
-                int pre_row = FindLastQueenRow(chess_board,col-1);
-                chess_board[pre_row][col-1]=0;// Previous queen is removed
-                FindAllArrangements(chess_board,ans,pre_row+1,col-1);
+                chess_board[i][col] = 1;
+                if(Solve(chess_board,col+1)){
+                    chess_board[i][col] = 0;
+                    return true;
+                }
+                chess_board[i][col] = 0;
             }
         }
-
+        return false;
     }
-
-    private static void Initialize(int[][] chess_board) {
-        for (int[] row : chess_board)
-            Arrays.fill(row, 0);
-    }
-
-    private static int FindLastQueenRow(int[][] chess_board, int col) {
-        for(int i = 0;i<chess_board[0].length;i++){
-            if(chess_board[i][col]==1){
-                return i;
-            }
-        }
-        return chess_board[0].length-1;
-    }
-
 
     public static void main(String []args){
         int N;
@@ -88,9 +58,7 @@ public class Problem_38 {
         System.out.println("Enter the value of N");
         N = sc.nextInt();
         int chess_board[][] = new int[N][N];
-        Count ans = new Count();
-        FindAllArrangements(chess_board,ans,0,0);
-        System.out.println("The number of possible arrangements is "+ans.count);
+        int ans = FindAllArrangements(chess_board,0,0);
+        System.out.println("The number of possible arrangements is "+ans);
     }
-
 }
